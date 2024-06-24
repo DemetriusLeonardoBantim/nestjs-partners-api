03d72b0d61d1f24efb6799d1f0e9876eedb012c5
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-06-24 16:55:12 -0300

    chore: connect to database

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  onModuleInit() {
+    throw new Error('Method not implemented.');
+  }
+  async OnModuleInit() {
+    this.$connect();
+  }
+}
