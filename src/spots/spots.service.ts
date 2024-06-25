fee3a65fe742b1020332034ed5cdbc1a307ad529
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-06-25 08:21:25 -0300

    feat: use PrismaService in SpotsService

--- src/spots/spots.service.ts
+++ src/spots/spots.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSpotDto } from './dto/create-spot.dto';
 import { UpdateSpotDto } from './dto/update-spot.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SpotsService {
+  constructor(private prismaService: PrismaService){}
+
   create(createSpotDto: CreateSpotDto) {
     return 'This action adds a new spot';
   }
